b38ca20c3ed5ffb87d54dfb572722c4087f7f523
Daria <daria@192.168.1.10>
2024-03-11 15:06:47 +0100

    fix formatting

--- cypress/integration/sorting.js
+++ cypress/integration/sorting.js
@@ -7,23 +7,24 @@ describe("Sorting and filtering", () => {
         for (let i = 0; i < count; i++) {
             cy.get("#product-sort-and-filter-bar .selectric-filter .selectric").click();
             cy.get("#product-sort-and-filter-bar .selectric-filter .selectric-scroll").then(options => {
-            expect(options).to.be.visible})
+                expect(options).to.be.visible
+            })
             cy.get("#product-sort-and-filter-bar .selectric-filter .selectric-scroll").find("li").eq(i).click();
             cy.get("#product-sort-and-filter-bar .selectric-filter .label").invoke("text").then(labelText => {
-            cy.log("text")
-                })
-            }
-        })
+                cy.log("text")
+            })
+        }
     })
-    it.only("Should filtering", () =>{
-        cy.visit("/drogeria/bestsellery/")
-        cy.get('[href="https://www.gandalf.com.pl/drogeria/cialo/"]').click();
-        cy.get("#marktree7").find("input").then(checkbox=>{
-            cy.get(checkbox).eq(1).check({force: true})
-            cy.get(checkbox).eq(5).check({force: true})
+})
+it.only("Should filtering", () => {
+    cy.visit("/drogeria/bestsellery/")
+    cy.get('[href="https://www.gandalf.com.pl/drogeria/cialo/"]').click();
+    cy.get("#marktree7").find("input").then(checkbox => {
+        cy.get(checkbox).eq(1).check({ force: true })
+        cy.get(checkbox).eq(5).check({ force: true })
         cy.get('[name="Filtruj"]').click()
-        cy.get(".filter-blocks").then(filter =>{
+        cy.get(".filter-blocks").then(filter => {
             expect(filter).to.be.visible
         })
-        })
     })
+})
