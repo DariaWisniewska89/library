451d215636baf6bd56f35de834758f28ce3d5d70
Daria <daria@192.168.1.10>
2024-04-04 09:51:32 +0200

    download form

--- cypress/integration/returns.js
+++ cypress/integration/returns.js
@@ -4,6 +4,17 @@ describe("Returns and complaints", () => {
     it("Should provide information on returns and complaints", () => {
         cy.visit("/kontakty");
         cy.get("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
-        cy.get('[style="background-image: url(/i/contacts/platnosc.svg);"]').click()
+        cy.get('[style="background-image: url(/i/contacts/zwroty.svg);"]').click()
+    })
+    it.only("Should download documents", () => {
+        cy.visit("/kontakty");
+        cy.get("#CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll").click()
+        cy.get('[style="background-image: url(/i/contacts/zwroty.svg);"]').click()
+        cy.get("#i0qmvv").click()
+        cy.visit("https://www.gandalf.com.pl/pomoc/reklamacje/")
+        cy.get("#ixmvrn").click()
+        cy.visit("https://www.gandalf.com.pl/pomoc/reklamacje/")
+        cy.get("#ind6xa").click()
+
     })
 })
